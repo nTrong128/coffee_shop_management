57e97eedbc14576f5b99090bac7cb29b39c4b211
vitris128 <lenhattrong128@gmail.com>
2024-01-18 01:40:46 +0700

    Update project to use JS and add Log In page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form";
+import * as z from "zod";
+
+import {Button} from "@/components/ui/button";
+import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+
+const formSchema = z
+  .object({
+    username: z.string().min(6, {
+      message: "Username must be at least 6 characters.",
+    }),
+
+    password: z.string(),
+  })
+  .superRefine((data, ctx) => {
+    if (data.password.length < 8) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Password must be at least 8 characters long",
+        path: ["password"],
+      });
+    }
+    if (!data.password.match(/[A-Z]/)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Password must contain at least one uppercase letter",
+        path: ["password"],
+      });
+    }
+    if (!data.password.match(/[a-z]/)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Password must contain at least one lowercase letter",
+        path: ["password"],
+      });
+    }
+    if (!data.password.match(/[0-9]/)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Password must contain at least one number",
+        path: ["password"],
+      });
+    }
+    if (!data.password.match(/[^A-Za-z0-9]/)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Password must contain at least one symbol",
+        path: ["password"],
+      });
+    }
+  });
+
+export default function Login() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="max-w-md w-full flex flex-col gap-4">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input required placeholder="Enter username" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input required autoComplete="off" placeholder="Enter password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit">LOG IN</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
