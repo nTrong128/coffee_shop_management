aa7ee49d529e01c6a01c860a82dc1bdc760ac250
vitris128 <lenhattrong128@gmail.com>
2024-01-25 02:29:38 +0700

    refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,18 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: 'Coffee Shop Management System',
-  description: 'Coffee Shop Management System',
-}
+  title: "Coffee Shop Management System",
+  description: "Coffee Shop Management System",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
