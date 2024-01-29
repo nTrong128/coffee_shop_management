edcbaa510337177257a25da6b91471c4e6c2d01c
vitris128 <lenhattrong128@gmail.com>
2024-01-29 20:14:29 +0700

    add homepage

---
+++ app/(dashboard)/user/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="text-center flex  flex-col text-slate-100 items-center justify-center bg-cover bg-center bg-no-repeat w-screen h-screen">
+      <h1 className="text-4xl py-4">Chào mừng bạn đến với Hệ Thống Quản Lý Cửa Hàng Cà Phê</h1>
+      <p className="text-lg py-4">Quản lý cửa hàng của bạn một cách dễ dàng, nhanh chóng!</p>
+      <h1 className="text-9xl py-4">LOGGED IN</h1>
+    </main>
+  );
+}
