7be170273feaaa6ccf428802c6fbb6e872abdb82
vitris128 <lenhattrong128@gmail.com>
2024-01-29 20:13:19 +0700

    db: prisma and queries

---
+++ schemas/index.ts
@@ -0,0 +1,58 @@
+import * as z from 'zod';
+
+export const RegisterSchema = z.object({
+    email: z.string().email().min(6, {
+      message: "Tài khoản phải từ 6 ký tự.",
+    }),
+
+  password: z.string(),
+  name: z.string().min(4, {
+    message: "Tên phải từ 4 ký tự.",
+  }),
+  })
+  .superRefine((data, ctx) => {
+    if (data.password.length < 8) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Mật khẩu phải từ 8 ký tự.",
+        path: ["password"],
+      });
+    }
+    if (!data.password.match(/[A-Z]/)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Mật khẩu phải có ít nhất 1 chữ hoa.",
+        path: ["password"],
+      });
+    }
+    if (!data.password.match(/[a-z]/)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Mật khẩu phải có ít nhất 1 chữ thường.",
+        path: ["password"],
+      });
+    }
+    if (!data.password.match(/[0-9]/)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Mật khẩu phải có ít nhất 1 số.",
+        path: ["password"],
+      });
+    }
+    if (!data.password.match(/[^A-Za-z0-9]/)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Mật khẩu phải có ít nhất 1 ký tự đặc biệt.",
+        path: ["password"],
+      });
+    }
+  });
+
+export const LoginSchema = z.object({
+    email: z.string().email().min(1,{
+        message: "Vui lòng nhập email.",
+    }),
+    password: z.string().min(1,{
+        message: "Vui lòng nhập mật khẩu.",
+    }),
+})
