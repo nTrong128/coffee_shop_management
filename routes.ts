3d96e2693436139c75e53d1babaaaabff1f91110
vitris128 <lenhattrong128@gmail.com>
2024-01-29 20:09:14 +0700

    add: Authentication and Middleware

---
+++ routes.ts
@@ -0,0 +1,32 @@
+/**
+ * An array of routes that are accessible to the public.
+ * These routes do not require authentication.
+ * @type {string[]}
+ */
+
+export const publicRoutes = [
+    "/",
+    "/login",
+];
+
+/**
+ * An array of routes that are accessible to authenticated users.
+ * These routes will redirect to the login page if the user is not authenticated.
+ * @type {string[]}
+ */
+export const authRoutes = [
+    "/login",
+    "/register",
+];
+/**
+ * the prefix for  API authentication routes.
+ * Routes that start with this prefix are used for API authentication purposes.
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect path after logging in.
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/dashboard";
