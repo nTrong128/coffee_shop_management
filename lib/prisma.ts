be18ade874319f1572c812723abc5d02c92f5fd8
vitris128 <lenhattrong128@gmail.com>
2024-02-02 10:47:33 +0700

    change: @prisma/client to @prisma/client/edge

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,10 @@
-import { PrismaClient } from '@prisma/client'
+import {PrismaClient} from "@prisma/client/edge";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+export const prisma = globalThis.prisma || new PrismaClient();
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
